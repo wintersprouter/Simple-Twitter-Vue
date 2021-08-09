928fdf282273cfe78a49b43a32db446caf219fc3
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-09 23:56:11 +0800

    style: modify navbar ui

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,46 +1,98 @@
 <template>
-  <v-card height="100vh" width="335" absolute left tile>
-    <v-container>
-      <v-list
-        flat
-        rounded
-        nav
-        class="ml-15 pl-10 pr-10 d-flex flex-column justify-space-around"
-      >
-        <!-- user -->
-        <v-list-item-group v-model="selectedItem" color="primary">
-          <!-- logo -->
-          <router-link to="/">
-            <v-btn icon color="primary" class="mx-4">
-              <v-img src="../assets/img/logo.svg" max-width="20"></v-img>
-            </v-btn>
-          </router-link>
-          <!-- logo-end -->
-          <v-list-item
-            v-for="userOption in userOptions"
-            :key="userOption.id"
-            link
-            class="mt-5"
+  <v-card
+    height="100vh"
+    width="235"
+    absolute
+    tile
+    class="d-flex flex-column justify-space-between"
+  >
+    <v-list flat rounded nav>
+      <!-- user -->
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <!-- logo -->
+        <router-link to="/">
+          <v-btn
+            icon
+            color="primary"
+            rounded
+            class="ml-1"
+            width="70px"
+            height="45px"
           >
-            <v-list-item-icon class="mx-4">
-              <v-img :src="userOption.icon" :alt="userOption.name"></v-img>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title
-                v-text="userOption.title"
-                class="font-weight-bold text-left"
-              ></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-btn block color="primary" dark class="mt-2 rounded-pill"
-            ><v-list-item-title class="font-weight-medium">
-              推文</v-list-item-title
-            >
+            <v-img
+              src="../assets/img/logo.svg"
+              max-width="40px"
+              max-height="40px"
+            ></v-img>
           </v-btn>
-        </v-list-item-group>
-        <!-- user-end -->
-
-        <!-- logout -->
+        </router-link>
+        <!-- logo-end -->
+        <v-list-item
+          v-for="userOption in userOptions"
+          :key="userOption.id"
+          link
+          class="mt-5 pr-5"
+        >
+          <v-list-item-icon class="ml-3">
+            <v-img :src="userOption.icon" :alt="userOption.name"></v-img>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title
+              v-text="userOption.title"
+              class="font-weight-bold text-left"
+            ></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item class="mt-3">
+          <v-btn block rounded color="primary" dark
+            ><v-list-item-title class="font-weight-medium" v-text="'推文'">
+            </v-list-item-title>
+          </v-btn>
+        </v-list-item>
+      </v-list-item-group>
+      <!-- user-end -->
+      <!-- admin -->
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <!-- logo -->
+        <router-link to="/">
+          <v-btn
+            icon
+            color="primary"
+            rounded
+            class="ml-1"
+            width="70px"
+            height="45px"
+          >
+            <v-img
+              src="../assets/img/logo.svg"
+              max-width="40px"
+              max-height="40px"
+            ></v-img>
+          </v-btn>
+        </router-link>
+        <!-- logo-end -->
+        <v-list-item
+          v-for="adminOption in adminOptions"
+          :key="adminOption.id"
+          link
+          class="mt-5 pr-5"
+        >
+          <v-list-item-icon class="ml-3">
+            <v-img :src="adminOption.icon" :alt="adminOption.name"></v-img>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title
+              v-text="adminOption.title"
+              class="font-weight-bold text-left"
+            ></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <!-- admin-end -->
+    </v-list>
+    <!-- logout -->
+    <v-list flat rounded nav>
+      <v-list-item-group v-model="selectedItem" color="primary">
         <v-list-item>
           <v-list-item-icon class="mx-4">
             <v-img src="../assets/img/logout.svg" max-width="20"></v-img>
@@ -51,9 +103,9 @@
             </v-list-item-content>
           </v-list-item-icon>
         </v-list-item>
-        <!-- logout -->
-      </v-list>
-    </v-container>
+      </v-list-item-group>
+    </v-list>
+    <!-- logout -->
   </v-card>
 </template>
 <script>
@@ -69,33 +121,28 @@ export default {
         {
           id: 1,
           icon: home,
-          name: "homepage",
           title: "首頁",
         },
         {
           id: 2,
           icon: user,
-          name: "user-info",
           title: "個人首頁",
         },
         {
           id: 3,
           icon: setting,
-          name: "setting",
           title: "設定",
         },
       ],
-      adminOptons: [
+      adminOptions: [
         {
           id: 1,
-          icon: "../assets/img/home.svg",
-          name: "tweets-list",
+          icon: home,
           title: "推文清單",
         },
         {
           id: 2,
-          icon: "../assets/img/user.svg",
-          name: "users-list",
+          icon: user,
           title: "使用者列表",
         },
       ],
