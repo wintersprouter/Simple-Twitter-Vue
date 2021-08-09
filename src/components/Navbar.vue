22162472e41583d179adcd2371059336b35eea1c
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-09 22:41:27 +0800

    feat: import navbar icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,80 +1,105 @@
 <template>
-  <nav>
-    <div class="container">
+  <v-card height="100vh" width="335" absolute left tile>
+    <v-container>
+      <v-list
+        flat
+        rounded
+        nav
+        class="ml-15 pl-10 pr-10 d-flex flex-column justify-space-around"
+      >
+        <!-- user -->
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <!-- logo -->
+          <router-link to="/">
+            <v-btn icon color="primary" class="mx-4">
+              <v-img src="../assets/img/logo.svg" max-width="20"></v-img>
+            </v-btn>
+          </router-link>
+          <!-- logo-end -->
+          <v-list-item
+            v-for="userOption in userOptions"
+            :key="userOption.id"
+            link
+            class="mt-5"
+          >
+            <v-list-item-icon class="mx-4">
+              <v-img :src="userOption.icon" :alt="userOption.name"></v-img>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title
+                v-text="userOption.title"
+                class="font-weight-bold text-left"
+              ></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-btn block color="primary" dark class="mt-2 rounded-pill"
+            ><v-list-item-title class="font-weight-medium">
+              推文</v-list-item-title
+            >
+          </v-btn>
+        </v-list-item-group>
+        <!-- user-end -->
 
-      <ul class="nav__list nav__list--brand">
-        <v-btn icon color="primary" class="nav__brand__link">
-          <v-icon class="nav__brand__icon" >mdi-twitter</v-icon>
-        </v-btn>
-      </ul>
-
-
-      <ul class="nav__list nav__list--user">
-        <li  v-for="userOption in userOptins" :key="userOption.id" class="nav__list__item">
-          <!-- <router-link class="nav__list__item__link"> -->
-            <img src="" alt="" class="nav__list__item__link__icon" :id="userOption.name">
-            <div class="nav__list__item__link__title">{{userOption.title}}</div>
-          <!-- </router-link> -->
-        </li>
-        <li class="nav__list__item">
-          <button class="nav__list__item__btn">推文</button>
-        </li>
-      </ul>
-
-      <ul class="nav__list nav__list--admin">
-        <li v-for=" adminOption in adminOptins" :key="adminOption.id" class="nav__list__item">
-          <!-- <router-link class="nav__list__item__link"> -->
-            <img src="" alt="" class="nav__list__item__link__icon" :id="adminOption.name">
-            <div class="nav__list__item__link__title">{{adminOption.title}}</div>
-          <!-- </router-link> -->
-        </li>
-      </ul>
-
-      <ul class="nav__list nav__list--logout">
-        <img src="" alt="" class="nav__list__item__link__icon">
-        <div class="nav__list__item__link__title">登出</div>
-      </ul>
-
-    </div>
-  </nav>
-
-
+        <!-- logout -->
+        <v-list-item>
+          <v-list-item-icon class="mx-4">
+            <v-img src="../assets/img/logout.svg" max-width="20"></v-img>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold text-left ml-4"
+                >登出</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item-icon>
+        </v-list-item>
+        <!-- logout -->
+      </v-list>
+    </v-container>
+  </v-card>
 </template>
 <script>
-  export default {
-    name: 'Navbar',
-    data: () => {
-      return {
-        userOptins: [
-          {
-            id:1,
-            name:'homepage',
-            title: '首頁',
-          },
-          {
-            id:2,
-            name:'user-info',
-            title: '個人首頁',
-          },
-          {
-            id:3,
-            name:'setting',
-            title: '設定',
-          },
-        ],
-        adminOptins: [
-          {
-            id:1,
-            name:'tweets-list',
-            title: '推文清單',
-          },
-          {
-            id:2,
-            name:'users-list',
-            title: '使用者列表',
-          }
-        ],
-      }
-    },
-  }
+import home from "./../assets/img/home.svg";
+import user from "./../assets/img/user.svg";
+import setting from "./../assets/img/setting.svg";
+export default {
+  name: "Navbar",
+  data: () => {
+    return {
+      selectedItem: 1,
+      userOptions: [
+        {
+          id: 1,
+          icon: home,
+          name: "homepage",
+          title: "首頁",
+        },
+        {
+          id: 2,
+          icon: user,
+          name: "user-info",
+          title: "個人首頁",
+        },
+        {
+          id: 3,
+          icon: setting,
+          name: "setting",
+          title: "設定",
+        },
+      ],
+      adminOptons: [
+        {
+          id: 1,
+          icon: "../assets/img/home.svg",
+          name: "tweets-list",
+          title: "推文清單",
+        },
+        {
+          id: 2,
+          icon: "../assets/img/user.svg",
+          name: "users-list",
+          title: "使用者列表",
+        },
+      ],
+    };
+  },
+};
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,13 +4,24 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  breakpoint: {
+    scrollBarWidth: 16,
+    thresholds: {
+      xs: 600,
+      sm: 960,
+      md: 1280,
+      lg: 1920
+    }
+  },
   theme: {
+    dark: false,
+    default: 'light',
+    disable: false,
     themes: {
       light: {
         primary: '#1DA1F2',
         secondary: '#F5F8FA',
-        accent: '#657786',
-        font: ' #1C1C1C'
+        accent: '#1C1C1C'
       }
     }
   }
