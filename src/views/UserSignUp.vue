3cd2d153c93bec5b9dcf94cf396a82d7b8ced96d
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-11 20:39:50 +0800

    feat: add UserForm UserSignUp vue

---
+++ src/views/UserSignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1>Sign Up</h1>
+    <section><UserForm /></section>
+  </v-container>
+</template>
+<script>
+import UserForm from "./../assets/scss/components/UserForm.vue";
+export default {
+  name: "UserSignUp",
+  component: {
+    UserForm,
+  },
+};
+</script>
