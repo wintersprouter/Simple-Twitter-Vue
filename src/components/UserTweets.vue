395391b7a279823ee7fd29e66c22129bab2b6ea3
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-11 16:32:00 +0800

    style: modify tweet card detail

--- src/components/UserTweets.vue
+++ src/components/UserTweets.vue
@@ -5,13 +5,13 @@
         <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="MasterCard" />
       </v-avatar>
       <v-list class="tweet-card-list">
-        <v-card-text id="tweet-card-text">
+        <v-card-text class="py-0">
           <v-list-item-group>
             <v-list-item-title>
               <span class="mr-2">MasterCard</span>
               <span>@MasterCard</span> <span> · </span> <span> 3 小時</span>
             </v-list-item-title>
-            <v-list-item-content>
+            <v-list-item-content class="pt-2 pb-0">
               Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
               ullamco cillum dolor. Voluptate exercitation incididunt aliquip
               deserunt reprehenderit elit laborum.
@@ -19,7 +19,7 @@
           </v-list-item-group>
         </v-card-text>
 
-        <v-card-actions id="tweet-card-actions">
+        <v-card-actions class="py-0">
           <v-list-item>
             <v-btn icon color="pink">
               <v-icon>mdi-heart-outline</v-icon>
