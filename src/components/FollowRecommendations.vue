34a25464e7e3146a3f48aee37fc956ebb58b3c85
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-11 16:13:46 +0800

    style: modify home layout

--- src/components/FollowRecommendations.vue
+++ src/components/FollowRecommendations.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-container>
-    <v-card class="secondary rounded-t-xl" max-width="350" outlined>
-      <v-card-title class="text-left card-list-title">跟隨誰</v-card-title>
+  <v-card elevation="0" max-width="350" class="recommend-list">
+    <v-card class="secondary rounded-t-xl" outlined>
+      <v-card-title class="text-left recommend-list-top-title"
+        >跟隨誰</v-card-title
+      >
     </v-card>
     <v-card class="secondary" max-width="350" outlined>
       <v-divider></v-divider>
       <v-row align="center" justify="space-around">
-        <v-list-item two-line class="ma-2">
+        <v-list-item two-line class="ma-2" width="100%">
           <v-list-item-avatar
             class="user-info-avatar"
             color="warning lighten-2"
@@ -26,18 +28,84 @@
             >
           </v-list-item-content>
           <v-card-actions class="followship-button">
-            <v-btn rounded color="primary" dark> 正在跟隨 </v-btn>
+            <!-- <v-btn rounded color="primary" elevation="0" dark class="px-4">
+                正在跟隨
+              </v-btn> -->
+            <v-btn rounded outlined color="primary" dark> 跟隨</v-btn>
+          </v-card-actions>
+        </v-list-item>
+      </v-row>
+    </v-card>
+    <v-card class="secondary" max-width="350" outlined>
+      <v-divider></v-divider>
+      <v-row align="center" justify="space-around">
+        <v-list-item two-line class="ma-2" width="100%">
+          <v-list-item-avatar
+            class="user-info-avatar"
+            color="warning lighten-2"
+            size="50"
+          >
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="MasterCard"
+            />
+          </v-list-item-avatar>
+          <v-list-item-content class="user-info-text">
+            <v-list-item-title class="user-info-text-name"
+              >MasterCard</v-list-item-title
+            >
+            <v-list-item-subtitle class="user-info-text-account"
+              >@MasterCard</v-list-item-subtitle
+            >
+          </v-list-item-content>
+          <v-card-actions class="followship-button">
+            <v-btn rounded color="primary" elevation="0" dark class="px-4">
+              正在跟隨
+            </v-btn>
             <!-- <v-btn rounded outlined color="primary" dark> 跟隨</v-btn> -->
           </v-card-actions>
         </v-list-item>
       </v-row>
+    </v-card>
+    <v-card class="secondary" max-width="350" outlined>
       <v-divider></v-divider>
+      <v-row align="center" justify="space-around">
+        <v-list-item two-line class="ma-2" width="100%">
+          <v-list-item-avatar
+            class="user-info-avatar"
+            color="warning lighten-2"
+            size="50"
+          >
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="MasterCard"
+            />
+          </v-list-item-avatar>
+          <v-list-item-content class="user-info-text">
+            <v-list-item-title class="user-info-text-name"
+              >MasterCard</v-list-item-title
+            >
+            <v-list-item-subtitle class="user-info-text-account"
+              >@MasterCard</v-list-item-subtitle
+            >
+          </v-list-item-content>
+          <v-card-actions class="followship-button">
+            <v-btn rounded color="primary" elevation="0" dark class="px-4">
+              正在跟隨
+            </v-btn>
+            <!-- <v-btn rounded outlined color="primary" dark> 跟隨</v-btn> -->
+          </v-card-actions>
+        </v-list-item>
+      </v-row>
     </v-card>
-    <v-card class="text-left secondary rounded-b-xl" max-width="350" outlined>
-      <v-card-text id="show-more-cards">顯示更多</v-card-text>
+
+    <v-card class="text-left secondary rounded-b-xl" outlined>
+      <v-divider></v-divider>
+      <v-card-text id="recommend-list-bottom-title">顯示更多</v-card-text>
     </v-card>
-  </v-container>
+  </v-card>
 </template>
+
 <script>
 export default {
   name: "FollowRecommendations",
---
+++ src/components/UserTweets.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card elevation="0" outlined tile class="tweet-list">
+    <v-card elevation="0" class="tweet-card">
+      <v-avatar size="50" class="tweet-card-avatar">
+        <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="MasterCard" />
+      </v-avatar>
+      <v-list class="tweet-card-list">
+        <v-card-text id="tweet-card-text">
+          <v-list-item-group>
+            <v-list-item-title>
+              <span class="mr-2">MasterCard</span>
+              <span>@MasterCard</span> <span> · </span> <span> 3 小時</span>
+            </v-list-item-title>
+            <v-list-item-content>
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.
+            </v-list-item-content>
+          </v-list-item-group>
+        </v-card-text>
+
+        <v-card-actions id="tweet-card-actions">
+          <v-list-item>
+            <v-btn icon color="pink">
+              <v-icon>mdi-heart-outline</v-icon>
+              <!-- <v-btn icon color="pink">
+          <v-icon>mdi-heart</v-icon>
+        </v-btn> -->
+            </v-btn>
+            <span>17</span>
+          </v-list-item>
+          <v-list-item>
+            <v-btn icon color="pink">
+              <v-icon>mdi-message-reply-outline</v-icon>
+            </v-btn>
+            <span>545</span>
+          </v-list-item>
+        </v-card-actions>
+      </v-list>
+    </v-card>
+    <v-divider></v-divider>
+  </v-card>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+@import "./../assets/scss/components/_UserTweets.scss";
+</style>
