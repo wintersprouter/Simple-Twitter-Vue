aa1668c66cbf28a6c5b8b55a1ac83c241bbd65ac
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-12 14:26:26 +0800

    feat: rename UserTweets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row>
         <section class="left-section"><Navbar /></section>
-        <section class="middle-section"><Tweets /></section>
+        <section class="middle-section"><UserTweets /></section>
         <section class="right-section"><FollowRecommendations /></section>
       </v-row>
     </v-container>
@@ -13,13 +13,13 @@
 <script>
 import Navbar from "./../components/Navbar";
 import FollowRecommendations from "./../components/FollowRecommendations";
-import Tweets from "./../components/Tweets";
+import UserTweets from "./../components/UserTweets";
 
 export default {
   name: "Home",
   components: {
     Navbar,
-    Tweets,
+    UserTweets,
     FollowRecommendations,
   },
 };
