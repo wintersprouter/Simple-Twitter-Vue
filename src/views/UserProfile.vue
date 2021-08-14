6ab62f3f9379bd6190e8063a9e4a59d9dc7a8e78
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-15 00:12:42 +0800

    feat: add header in profile page

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -3,6 +3,22 @@
     <v-row>
       <section class="left-section"><Navbar /></section>
       <section class="middle-section">
+        <v-card elevation="0" height="55px">
+          <v-container class="d-flex pt-1">
+            <v-btn icon>
+              <v-icon color="black">mdi-arrow-left</v-icon>
+            </v-btn>
+            <v-text class="ml-3">
+              <v-list-item-title class="header-user-title"
+                >John Doe</v-list-item-title
+              >
+              <v-list-item-subtitle class="header-user-subtitle"
+                >25 推文
+              </v-list-item-subtitle>
+            </v-text>
+          </v-container>
+        </v-card>
+        <v-divider></v-divider>
         <UserProfileInfo /><UserProfileNav /> <v-divider></v-divider
         ><UserTweets />
       </section>
