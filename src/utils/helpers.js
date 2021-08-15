258cbe6baa781f8bb8bb0dbc70a8fe334d5c18d8
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-15 15:25:40 +0800

    feat: install sweetalert2

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const baseURL = 'https://simpletwitter-api.herokuapp.com/api/'
 
 export const apiHelper = axios.create({
   baseURL
+})
+export const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
 })
