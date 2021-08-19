410ee778612f8562650afcdf8e1d7c1f04997deb
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-19 20:14:35 +0800

    feat: fetch getUserRepliedTweets api

--- src/components/UserRepliedTweets.vue
+++ src/components/UserRepliedTweets.vue
@@ -1,66 +1,54 @@
 <template>
-  <v-card elevation="0" relative>
-    <v-list class="p-0">
-      <v-container class="profile-reply-card">
-        <v-avatar size="50" class="pt-0">
-          <img
-            src="https://cdn.vuetifyjs.com/images/john.jpg"
-            alt="MasterCard"
-          />
-        </v-avatar>
-        <v-divider vertical inset class="my-2 connect-line"></v-divider>
-
-        <v-card-text class="py-0">
-          <v-list-item-title>
-            <span class="mr-2 tweets-name">MasterCard</span>
-            <span class="tweets-account">@MasterCard</span>
-            <span class="tweets-account"> · </span>
-            <span class="tweets-account"> 3 小時</span>
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            <span class="tweet-reply-title mr-1">回覆給</span>
-            <span class="tweet-reply-target"
-              >@aomorm</span
-            ></v-list-item-subtitle
-          >
-          <v-list-item-content class="pt-2 pb-2">
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum.
-          </v-list-item-content>
-        </v-card-text>
-      </v-container>
-      <v-container class="profile-reply-card">
-        <v-avatar size="50" class="tweet-card-avatar">
-          <img
-            src="https://cdn.vuetifyjs.com/images/john.jpg"
-            alt="MasterCard"
-          />
-        </v-avatar>
-        <v-card-text class="py-0">
-          <v-list-item-title>
-            <span class="mr-2 tweets-name">MasterCard</span>
-            <span class="tweets-account">@MasterCard</span>
-            <span class="tweets-account"> · </span>
-            <span class="tweets-account"> 3 小時</span>
-          </v-list-item-title>
-          <v-list-item-content class="pt-2 pb-2">
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum.
-          </v-list-item-content>
-        </v-card-text>
-      </v-container>
-    </v-list>
-    <v-divider></v-divider>
-  </v-card>
+  <section>
+    <UserRepliedTweetsCard
+      v-for="tweet in tweets"
+      :key="tweet.id"
+      :initial-tweet="tweet"
+    />
+  </section>
 </template>
 <script>
+import UserRepliedTweetsCard from "./UserRepliedTweetsCard";
+import userAPI from "../apis/users";
+import { Toast } from "./../utils/helpers";
+import { mapState } from "vuex";
+
 export default {
-  name: "UserRepiedweets",
+  name: "UserRepliedTweets",
+  data() {
+    return {
+      tweets: [],
+      isProcessing: false,
+    };
+  },
+  components: {
+    UserRepliedTweetsCard,
+  },
+  methods: {
+    async fetchUserRepliedTweets(userId) {
+      try {
+        const { data } = await userAPI.users.getUserRepliedTweets(userId);
+        this.tweets = data;
+        console.log("RepliedTweet", data);
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取得推文資料，請稍後再試",
+        });
+        console.log("error", error);
+      }
+    },
+  },
+  created() {
+    const { id } = this.$route.params;
+    this.fetchUserRepliedTweets(id);
+  },
+  computed: {
+    ...mapState(["currentUser"]),
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.fetchUserRepliedTweets(to.params.id);
+    next();
+  },
 };
 </script>
-<style lang="scss">
-@import "../assets/scss/components/_UserRepiedTweets.scss";
-@import "../assets/scss/components/_ReplyTweetModal.scss";
-</style>
