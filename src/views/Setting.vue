8ff68576476817054af17feb176804fdb2fce2b2
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-21 20:13:56 +0800

    feat: add setting route

---
+++ src/views/Setting.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row>
+      <section class="left-section"><Navbar /></section>
+      <section class="main-section"><UserForm /></section>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import Navbar from "./../components/Navbar";
+import UserForm from "./../components/UserForm.vue";
+import { mapState } from "vuex";
+export default {
+  name: "Setting",
+  components: {
+    Navbar,
+    UserForm,
+  },
+  computed: {
+    ...mapState(["currentUser", "isAuthenticated"]),
+  },
+};
+</script>
+
+
