30131fbd1ca6f177e83fcdbfd258e9901b53232d
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-24 16:28:33 +0800

    feat: modify 404 not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>404 Page Not Found</h1>
+  <v-card height="100vh" elevation="0">
+    <v-card-title class="mb-0 pb-0"
+      ><v-spacer></v-spacer>
+      <h2>404 Page Not Found</h2>
+      <v-spacer></v-spacer
+    ></v-card-title>
+    <v-img
+      src="../assets/img/404.svg"
+      width="80%"
+      height="80%"
+      class="mx-auto"
+    ></v-img>
+    <v-card-actions
+      ><v-spacer></v-spacer
+      ><v-btn elevation="2" x-large color="primary" @click="$router.back()">
+        Back
+      </v-btn></v-card-actions
+    >
+  </v-card>
 </template>
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
