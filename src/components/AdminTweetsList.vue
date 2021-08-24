443aa3268878563028eb2bede3bec02a9ec86fcb
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-24 14:15:04 +0800

    feat: use v-data-table

--- src/components/AdminTweetsList.vue
+++ src/components/AdminTweetsList.vue
@@ -5,36 +5,25 @@
     </v-card>
     <v-divider></v-divider>
     <v-container>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">推文Id</th>
-              <th class="text-left">作者Id</th>
-              <th class="text-left">作者帳號</th>
-              <th class="text-left">推文內容</th>
-              <th class="text-left">推文時間</th>
-              <th class="text-left">推文讚數</th>
-              <th class="text-left">推文回文數</th>
-              <th class="text-left">推文刪除</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="tweet in tweets" :key="tweet.id" :initial-tweet="tweet">
-              <td>{{ tweet.id }}</td>
-              <td>{{ tweet.UserId }}</td>
-              <td>@{{ tweet.account }}</td>
-              <td>{{ tweet.description | preview }}</td>
-              <td>{{ tweet.createdAt | format }}</td>
-              <td>{{ tweet.likedCount }}</td>
-              <td>{{ tweet.repliedCount }}</td>
-              <td>
-                <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-              </td>
-            </tr>
-          </tbody>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        class="elevation-1"
+        :search="search"
+        :headers="headers"
+        :items="tweets"
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
         </template>
-      </v-simple-table>
+      </v-data-table>
     </v-container>
   </v-card>
 </template>
@@ -42,16 +31,46 @@
 import tweetsAPI from "../apis/tweets";
 import { Toast } from "./../utils/helpers";
 // import AdminTweets from "../components/AdminTweets";
-import { fromNowFilter, tweetFilter } from "./../utils/mixins";
+import { fromNowFilter } from "./../utils/mixins";
+import moment from "moment";
 export default {
   name: "AdminTweetsList",
   // components: {
   //   AdminTweets,
   // },
-  mixins: [fromNowFilter, tweetFilter],
+  mixins: [fromNowFilter],
   data() {
     return {
-      tweets: [],
+      tweets: [
+        {
+          id: -1,
+          UserId: -1,
+          account: "",
+          description: "",
+          createdAt: "",
+          likedCount: "",
+          repliedCount: "",
+        },
+      ],
+      search: "",
+      headers: [
+        {
+          text: "推文Id",
+          value: "id",
+        },
+        { text: "作者Id", value: "UserId" },
+        { text: "作者帳號", value: "account", sortable: false },
+        {
+          text: "推文內容",
+          value: "description",
+          align: "start",
+          sortable: false,
+        },
+        { text: "推文時間", value: "createdAt" },
+        { text: "推文讚數", value: "likedCount" },
+        { text: "推文回文數", value: "repliedCount" },
+        { text: "Action", value: "action", sortable: false },
+      ],
     };
   },
   watch: {
@@ -66,7 +85,17 @@ export default {
     async fetchTweets() {
       try {
         const { data } = await tweetsAPI.getTweets();
-        this.tweets = data;
+        this.tweets = data.map((t) => {
+          return {
+            ...t,
+            createdAt: moment(t.createdAt).format("LLL"),
+            account: "@" + t.account,
+            description:
+              t.description.length > 50
+                ? t.description.substring(0, 50) + "..."
+                : t.description,
+          };
+        });
       } catch (error) {
         Toast.fire({
           icon: "error",
--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -4,20 +4,6 @@ export const fromNowFilter = {
   filters: {
     fromNow(datetime) {
       return datetime ? moment(datetime).fromNow() : '-'
-    },
-    format(datetime) {
-      return datetime ? moment(datetime).format('LLL') : '-'
-    }
-  }
-}
-export const tweetFilter = {
-  filters: {
-    preview(tweet) {
-      if (tweet.length > 50) {
-        return tweet.substring(0, 50) + '...'
-      } else {
-        return tweet
-      }
     }
   }
 }
