859a6d381b37986bb6523af63149619c2ca2889d
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-24 15:54:00 +0800

    feat: fetch delete tweet api

--- src/components/AdminTweetsList.vue
+++ src/components/AdminTweetsList.vue
@@ -22,8 +22,32 @@
         :loading="isLoading"
         loading-text="Loading... Please wait"
       >
-        <template v-slot:item.actions="{ item }">
-          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        <template v-slot:top>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5"
+                >Are you sure you want to delete this tweet?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Cancel</v-btn
+                >
+                <v-btn
+                  color="error"
+                  :loading="isLoading"
+                  class="ma-1"
+                  text
+                  @click="deleteItemConfirm"
+                  >Delete</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
+        <template v-slot:item.action="{ item }">
+          <v-icon small @click="deleteItem(item.id)"> mdi-delete </v-icon>
         </template>
       </v-data-table>
     </v-container>
@@ -32,14 +56,11 @@
 <script>
 import tweetsAPI from "../apis/tweets";
 import { Toast } from "./../utils/helpers";
-// import AdminTweets from "../components/AdminTweets";
+import adminAPI from "./../apis/admin";
 import { fromNowFilter } from "./../utils/mixins";
 import moment from "moment";
 export default {
   name: "AdminTweetsList",
-  // components: {
-  //   AdminTweets,
-  // },
   mixins: [fromNowFilter],
   data() {
     return {
@@ -57,28 +78,33 @@ export default {
       search: "",
       headers: [
         {
-          text: "推文Id",
+          text: "tweet id",
           value: "id",
         },
-        { text: "作者Id", value: "UserId" },
-        { text: "作者帳號", value: "account", sortable: false },
+        { text: "author id", value: "UserId" },
+        { text: "author account", value: "account", sortable: false },
         {
-          text: "推文內容",
+          text: "tweet description",
           value: "description",
           align: "start",
           sortable: false,
         },
-        { text: "推文時間", value: "createdAt" },
-        { text: "推文讚數", value: "likedCount" },
-        { text: "推文回文數", value: "repliedCount" },
-        { text: "Action", value: "action", sortable: false },
+        { text: "tweet createdAt", value: "createdAt" },
+        { text: "tweet likes", value: "likedCount" },
+        { text: "tweet replies", value: "repliedCount" },
+        { text: "action", value: "action", sortable: false },
       ],
       isLoading: false,
+      dialogDelete: false,
+      deleteItemId: -1,
     };
   },
   watch: {
     initialTweets(newValue) {
       this.tweets = [...newValue];
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
     },
   },
   created() {
@@ -109,6 +135,41 @@ export default {
         });
         console.log("error", error);
       }
+    },
+    deleteItem(id) {
+      this.deleteItemId = id;
+      this.dialogDelete = true;
+    },
+
+    async deleteItemConfirm() {
+      try {
+        let tweetId = this.deleteItemId;
+        this.isLoading = true;
+        const { data } = await adminAPI.deleteTweet({ tweetId });
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+        Toast.fire({
+          icon: "success",
+          title: `${data.message}`,
+        });
+        this.isLoading = false;
+        this.closeDelete();
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: "無法刪除此推文，請稍後再試",
+        });
+        this.isLoading = false;
+        this.closeDelete();
+      }
+    },
+    closeDelete() {
+      this.dialogDelete = false;
+      this.deleteItemId = -1;
     },
   },
 };
